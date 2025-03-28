258e64f504925088045fe0ca2d1a78b97eec7ee4
Andrei <raducanuandrei31@gmail.com>
2025-03-28 20:56:51 +0200

    Added e2e css test

--- zoom_bot/tests/e2e/test_css_selectors.py
+++ zoom_bot/tests/e2e/test_css_selectors.py
@@ -3,6 +3,6 @@ from zoom_bot.app import ZoomBot
 
 
 def test_css_selector_before_join():
-    zoom_bot = ZoomBot('1', 'dummy_pass', 'test_user', 1, False)
+    zoom_bot = ZoomBot('1', 'dummy_pass', 'test_user', 1, True)
     with pytest.raises(ValueError, match="Wrong password"):
         zoom_bot.join_meeting()
